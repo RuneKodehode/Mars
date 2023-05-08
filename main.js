910cf0b44b74acf7dad9c452223bba486685d51f
RuneKodehode <rune.kodehode@gmail.com>
2023-05-08 14:55:17 +0200

    Equipment

--- main.js
+++ main.js
@@ -4,20 +4,29 @@ const dateInput = document.getElementById("date-input");
 const iframe = document.getElementById("frame");
 const gps = document.getElementById("gps");
 const imageContainer = document.getElementById("image-container");
-let count = 0;
+const weatherButton = document.getElementById("weather");
+const equipBtn = document.getElementById("equipBtn");
+let check = 1;
+const equip = document.getElementById("equipment");
+
+// EVENTLISTENERS
+weatherButton.addEventListener("click", toggleWeather);
+gps.addEventListener("click", toggleGps);
+equipBtn.addEventListener("click", toggleEquip);
+// end of eventlisteners
+
 dateInput.addEventListener("change", async (event) => {
   const date = event.target.value;
   const apiEndpoint = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=${date}&api_key=nbOcqt4gNqe7vaqXhEVlZ2Xu4rHfu2IK68Xdvgov`;
-
   const response = await fetch(apiEndpoint);
   const data = await response.json();
   const imageUrls = data.photos.map((photo) => photo.img_src);
   document.getElementById(
     "sol"
   ).textContent = `Martian Sol: ${data.photos[0].sol}`;
-
   const imageContainer = document.getElementById("image-container");
   imageContainer.innerHTML = "";
+  equip.style.visibility = "hidden";
   iframe.style.visibility = "hidden";
   imageUrls.forEach((url) => {
     const img = document.createElement("img");
@@ -30,14 +39,6 @@ dateInput.addEventListener("change", async (event) => {
   });
 });
 
-const weatherButton = document.getElementById("weather");
-
-weatherButton.addEventListener("click", toggleWeather);
-
-console.log(iframe.style.visibility);
-
-let check = 1;
-
 function toggleWeather() {
   iframe.src = "https://mars.nasa.gov/layout/embed/image/mslweather/";
   if (iframe.style.visibility === "visible" && check === 2) {
@@ -46,10 +47,9 @@ function toggleWeather() {
     iframe.style.visibility = "visible";
     imageContainer.innerHTML = "";
     check = 2;
+    equip.style.visibility = "hidden";
   }
 }
-
-gps.addEventListener("click", toggleGps);
 
 function toggleGps() {
   iframe.src = "https://mars.nasa.gov/maps/location/?mission=MSL&site=NOW";
@@ -59,5 +59,12 @@ function toggleGps() {
     iframe.style.visibility = "visible";
     imageContainer.innerHTML = "";
     check = 1;
+    equip.style.visibility = "hidden";
   }
+}
+
+function toggleEquip() {
+  imageContainer.innerHTML = "";
+  equip.style.visibility = "visible";
+  iframe.style.visibility = "hidden";
 }
